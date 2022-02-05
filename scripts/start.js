db7d59e0d271b5a3a29561b8020fcbd4a5a1a822
Drakanov <creatica-creaciones@hotmail.com>
2022-02-05 11:32:52 +0100

    ready to deploy

--- scripts/start.js
+++ scripts/start.js
@@ -1,11 +1,11 @@
 var MAX_SUPPLY = null
-const CONTRACT_ADDRESS = "0xBfe7f07224c0cEB5B164649e92E7772E6D859B9A"
+const CONTRACT_ADDRESS = "0xd5ecD08A76b91A007C047DE17A1C0845B7CC5Cf3"
 const PORT = 3000
 const IS_REVEALED = true
 const UNREVEALED_METADATA = {
   "name":"Unrevealed Croc",
   "description":"???",
-  "image":"http://134.209.33.178:3000/unrevealed/image.png",
+  "image":"http://165.22.203.43:3000/unrevealed/image.png",
   "attributes":[{"???":"???"}]
 }
 
